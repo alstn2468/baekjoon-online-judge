a230e3385a77cd74258e594dec951dc87393c4e0
alstn2468 <alstn2468_@naver.com>
2018-03-11 19:57:53 +0900

    2018-03-11

--- 1900~1999/1929.c
+++ 1900~1999/1929.c
@@ -4,45 +4,34 @@
 	*	M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
 	*
 	*	입력
-	*	첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1≤M≤N≤1,000,000)
+	*	첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000)
 	*
 	*	출력
 	*	한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
 	*
 */
 
-#include<stdio.h>
+#include <stdio.h>
+
+#define MAX 1000001
 
 int main()
 {
 	int num1, num2;
+	int arr[MAX] = { 0,1 };
 
-	scanf("%d", &num1);
-	scanf("%d", &num2);
+	scanf("%d %d", &num1, &num2);
 
-	if (num1 > num2)
-		return 0;
+	for (int i = 2; i <= num2; i++)
+	{
+		for (int j = 2; i * j <= num2; j++)
+			arr[i * j] = 1;
+	}
 
 	for (int i = num1; i <= num2; i++)
 	{
-		int check = 0;
-
-		if (i == 1)
-			check++;
-
-		for (int j = 2; j < i; j++)
-		{
-			if (i % j == 0)
-			{
-				check++;
-				break;
-			}
-		}
-
-		if (check == 0)
-		{
+		if (!arr[i])
 			printf("%d\n", i);
-		}
 	}
 
 	return 0;
