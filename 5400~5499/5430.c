9cc3d2809feacc06ca0ba13b476902dae59d6d80
alstn2468 <alstn2468_@naver.com>
2018-04-20 00:07:26 +0900

    2018-04-20

--- 5400~5499/5430.c
+++ 5400~5499/5430.c
@@ -20,3 +20,88 @@
 	*	만약, 에러가 발생한 경우에는 error를 출력한다.
 	*
 */
+
+#include <stdio.h>
+
+void Sol(int *AC, int n, char *p)
+{
+	int dir = 0, front = 0, rear = n, flag = 0;
+
+	for (int i = 0; p[i]; i++)
+	{
+		switch (p[i])
+		{
+		case 'R':
+			if (!n)
+				flag = 1;
+			dir ^= 1;
+			break;
+		case 'D':
+			flag = 0;
+			if (rear <= front)
+			{
+				flag = 2;
+				goto exit;
+			}
+			if (dir)
+				rear--;
+			else
+				front++;
+			break;
+		}
+	}
+exit:
+	if (flag == 1)
+		puts("[]");
+	else if (!flag)
+	{
+		printf("[");
+		if (dir)
+		{
+			for (int i = rear - 1; i >= front; i--)
+			{
+				printf("%d", AC[i]);
+				if (i != front)
+					printf(",");
+			}
+		}
+		else
+		{
+			for (int i = front; i < rear; i++)
+			{
+				printf("%d", AC[i]);
+				if (i != rear - 1)
+					printf(",");
+			}
+		}
+		printf("]\n");
+	}
+	else
+		puts("error");
+}
+
+int main()
+{
+	int test_case, n, AC[100000];
+	char p[100001];
+
+	scanf("%d%*c", &test_case);
+
+	while (test_case--)
+	{
+		gets(p);
+		scanf("%d%*c%*c", &n);
+
+		if (!n)
+			scanf("%*c");
+
+		for (int i = 0; i < n; i++)
+			scanf("%d%*c", &AC[i]);
+
+		Sol(AC, n, p);
+
+		scanf("%*c");
+	}
+
+	return 0;
+}
