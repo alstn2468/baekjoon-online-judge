ef30c64e10b0f2310a4a0b5f5b40b73f7dcfebbd
alstn2468 <alstn2468_@naver.com>
2018-04-20 15:52:32 +0900

    2018-04-20

---
+++ 2700~2799/2747.c
@@ -0,0 +1,49 @@
+/*
+	*
+	*	문제
+	*	피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다.
+	*	그 다음 2번째 부터는 바로 앞 두 피보나치 수의 합이 된다.
+	*	이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n>=2)가 된다.
+	*	n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+	*	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+	*	n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+	*
+	*	입력
+	*	첫째 줄에 n이 주어진다. n은 45보다 작거나 같은 자연수이다.
+	*
+	*	출력
+	*	첫째 줄에 n번째 피보나치 수를 출력한다.
+	*
+*/
+
+#include <stdio.h>
+
+int fib(int input);
+
+int main()
+{
+	int input;
+
+	scanf("%d", &input);
+
+	printf("%d\n", fib(input));
+
+	return 0;
+}
+
+int fib(int input)
+{
+	if (input < 2)
+		return input;
+	else
+	{
+		int i, tmp, current = 1, last = 0;
+		for (i = 2; i <= input; i++)
+		{
+			tmp = current;
+			current += last;
+			last = tmp;
+		}
+		return current;
+	}
+}
