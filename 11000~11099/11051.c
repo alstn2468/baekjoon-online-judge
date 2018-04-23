cfdb277fb0e01a2e2fe7a56e5d93ba1f586993cf
alstn2468 <alstn2468_@naver.com>
2018-04-23 21:23:52 +0900

    2018-04-23

--- 11000~11099/11051.c
+++ 11000~11099/11051.c
@@ -10,3 +10,40 @@
 	*	(N K)를 10,007로 나눈 나머지를 출력한다.
 	*
 */
+
+#include <stdio.h>
+#define MOD 10007
+#define NMAX 1001
+
+typedef long long int ll;
+ll dp[NMAX][NMAX];
+
+void makePascalTriangle()
+{
+	dp[0][0] = 1;
+
+	for (int i = 1; i < NMAX; i++)
+	{
+		dp[i][0] = 1;
+
+		for (int j = 1; j <= i; j++)
+			dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
+	}
+}
+
+ll nCr(int n, int k)
+{
+	return dp[n][k];
+}
+
+int main()
+{
+	int n, k;
+
+	makePascalTriangle();
+
+	scanf("%d %d", &n, &k);
+	printf("%lld\n", nCr(n, k));
+
+	return 0;
+}
