2d51f406ba5e6d7a59be5012910725e8f2b272cd
alstn2468 <alstn2468_@naver.com>
2018-05-23 09:54:26 +0900

    2018-05-23

--- 2500~2599/2579.py
+++ 2500~2599/2579.py
@@ -23,3 +23,21 @@
 #
 # 출력
 # 첫째 줄에 계단 오르기 게임에서 얻을 수 있는 총 점수의 최대값을 출력한다.
+
+n = int(input())
+
+stair = []
+
+for i in range(n) :
+    stair.append(int(input()))
+
+sum = []
+
+sum.append(stair[0])
+sum.append(stair[1] + stair[0])
+sum.append(max(stair[2] + stair[0], stair[2] + stair[1]))
+
+for i in range(3, n) :
+    sum.append(max(stair[i] + sum[i - 2], stair[i] + stair[i - 1] + sum[i - 3]))
+
+print(sum[n - 1])
