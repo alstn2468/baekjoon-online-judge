1208f51badb8d2408f57fcad2c9af10a70f97346
alstn2468 <alstn2468_@naver.com>
2018-05-30 00:39:44 +0900

    2018-05-30

--- 10800~10899/10844.py
+++ 10800~10899/10844.py
@@ -14,31 +14,34 @@
 # 출력
 # 첫째 줄에 정답을 1,000,000,000으로 나눈 나머지를 출력한다.
 
+n = [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 
-dp = [[0 for j in range(10)] for i in range(102)]
+def stair(n)  :
 
-n = int(input())
+    output = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    output[0] = n[1]
+    output[9] = n[8]
 
-for i in range(10) :
+    for i in range(8) :
 
-    dp[1][i] = 1
+        j = i + 1
+        output[j] = n[j - 1] + n[j + 1]
 
-for i in range(2, n + 1) :
+    return output
 
-    for j in range(10) :
+def total(n) :
 
-        if j == 0 :
-            dp[i][j] = dp[i - 1][j] % 1000000000
+    total = 0
 
-        elif j == 9 :
-            dp[i][j] = dp[i - 1][8] % 1000000000
+    for i in n :
+        total += i
 
-        else :
-            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000
+    return total
 
-s = 0
 
-for i in range(1, 10) :
-    s = (s + dp[n][i]) % 1000000000
+length = input()
 
-print(s)
+for i in range(int(length) - 1) :
+    n = stair(n)
+
+print( total(n) % 1000000000 )
