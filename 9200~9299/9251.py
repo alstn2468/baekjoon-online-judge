269457bb21104196a68d83b1f24af0aed7739e33
alstn2468 <alstn2468_@naver.com>
2018-06-09 01:15:10 +0900

    2018-06-9

--- 9200~9299/9251.py
+++ 9200~9299/9251.py
@@ -11,29 +11,25 @@
 # 출력
 # 첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를 출력한다.
 
-def LCS(str1, str2) :
-
-    m = len(str1)
-    n = len(str2)
-
-    dp = [[0] * (n + 1) for i in range(m + 1)]
-
-    for i in range(m + 1) :
-
-        for j in range(n + 1) :
-
-            if i == 0 or j == 0 :
-                dp[i][j] = 0
-
-            elif str1[i - 1] == str2[j - 1] :
-                dp[i][j] = dp[i - 1][j - 1] + 1
-
-            else :
-                dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1])
-
-    return dp[m][n]
-
 str1 = input()
 str2 = input()
 
-print(LCS(str1, str2))
+m = len(str1)
+n = len(str2)
+
+dp = [[0] * (n + 1) for i in range(m + 1)]
+
+for i in range(m + 1) :
+
+    for j in range(n + 1) :
+
+        if i == 0 or j == 0 :
+            dp[i][j] = 0
+
+        elif str1[i - 1] == str2[j - 1] :
+            dp[i][j] = dp[i - 1][j - 1] + 1
+
+        else :
+            dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1])
+
+print(dp[m][n])
