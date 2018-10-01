8ec5e70fb523fcc87f6bc3598674e30cb2b793d8
alstn2468 <alstn2468_@naver.com>
2018-10-01 17:18:13 +0900

    2018-10-01

--- 1100~1199/1107.py
+++ 1100~1199/1107.py
@@ -20,3 +20,27 @@
 #
 # 출력
 # 첫째 줄에 채널 N으로 이동하기 위해 버튼을 최소 몇 번 눌러야 하는지를 출력한다.
+
+min_cnt = 0x3f3f3f3f
+num_int = 0
+btn_set = {i for i in range(10)}
+
+def find(num) :
+    global min_cnt, num_int
+
+    for btn in btn_set :
+        tmp_num = num + str(btn)
+        min_cnt = min(min_cnt, abs(num_int - int(tmp_num)) + len(tmp_num))
+
+        if len(tmp_num) < 6 :
+            find(tmp_num)
+
+num_int = int(input())
+n = int(input())
+
+min_cnt = min(min_cnt, abs(100 - num_int))
+btn_set -= set(map(int, input().split())) if n else set()
+
+find('') if n < 10 else ''
+
+print(min_cnt)
