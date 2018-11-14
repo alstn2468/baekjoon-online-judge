ad0e3669e308cebd5c95b9535415864e7fae7f5a
alstn2468 <alstn2468_@naver.com>
2018-11-14 14:13:50 +0900

    Complete 11004.py Why..??

--- 11000~11099/11004.py
+++ 11000~11099/11004.py
@@ -11,38 +11,7 @@
 # 출력
 # A를 정렬했을 때, 앞에서부터 K번째 있는 수를 출력한다.
 
-import random, sys
-
-def nth_element(arr, n) :
-    left, right = 0, len(arr) - 1
-
-    if not left <= n - 1 <= right :
-        raise ValueError('n(%d) must be in [1, %d]' % n, len(arr))
-
-    while left < right :
-        pivot_index = random.randint(left, right)
-        pivot_value = arr[pivot_index]
-        arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
-        store_index = left
-
-        for i in range(left, right) :
-            if arr[i] < pivot_value :
-                arr[store_index], arr[i] = arr[i], arr[store_index]
-                store_index += 1
-
-        arr[store_index], arr[right] = arr[right], arr[store_index]
-
-        if n - 1 == store_index :
-            return arr[store_index]
-
-        elif n - 1 < store_index :
-            right = store_index - 1
-
-        else :
-            left = store_index + 1
-
-    return arr[left]
-
+import sys
 
 N, K = map(int, sys.stdin.readline().split())
 A = sys.stdin.readline().split()
@@ -50,4 +19,6 @@ A = sys.stdin.readline().split()
 for i in range(len(A)) :
     A[i] = int(A[i])
 
-print(nth_element(A, K))
+A.sort()
+
+print(A[K - 1])
