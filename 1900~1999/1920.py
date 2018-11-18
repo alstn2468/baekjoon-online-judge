4e1ec1834fbd0f2b856c590a66b68139b09e1060
alstn2468 <alstn2468_@naver.com>
2018-11-19 00:49:23 +0900

    Solve 1920.py

---
+++ 1900~1999/1920.py
@@ -0,0 +1,44 @@
+# 문제
+# N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때,
+# 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 N(1≤N≤100,000)이 주어진다.
+# 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다.
+# 다음 줄에는 M(1≤M≤100,000)이 주어진다.
+# 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다.
+# 모든 정수들의 범위는 int 로 한다.
+
+# 출력
+# M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+import sys
+
+def binary_search(target, data):
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if data[mid] == target:
+            return True
+
+        elif data[mid] < target:
+            start = mid + 1
+
+        else:
+            end = mid -1
+
+    return False
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+
+M = int(sys.stdin.readline())
+B = list(map(int, sys.stdin.readline().split()))
+
+A.sort()
+
+for i in range(M):
+    print(1) if binary_search(B[i], A) else print(0)
