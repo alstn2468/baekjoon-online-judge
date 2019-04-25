95fec9c2f33abb69dcaca0a2551ce02ec3505648
alstn2468 <alstn2468_@naver.com>
2019-04-25 11:29:33 +0900

    Solve 10952.py A+B - 5

--- 10900~10999/10952.py
+++ 10900~10999/10952.py
@@ -3,12 +3,19 @@
 # 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 #
 # 입력
-# 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+# 입력은 여러 개의 테스트 케이스로 이루어져 있다.
 # 각 테스트 케이스는 한 줄로 이루어져 있으며,
 # 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+#
+# 입력의 마지막에는 0 두 개가 들어온다.
 #
 # 출력
 # 각 테스트 케이스마다 A+B를 출력한다.
 
-for _ in range(int(input())):
-    print(sum(list(map(int, input().split()))))
+while True:
+    a, b = map(int, input().split())
+
+    if a == 0 and b == 0:
+        break
+
+    print(a + b)
