f08317bcbcf050bd5a7d5d9597fb112140336fe4
alstn2468 <alstn2468_@naver.com>
2019-06-03 13:13:00 +0900

    Solve 11650 boj sorting postions

---
+++ 11600~11699/11650.py
@@ -0,0 +1,25 @@
+
+# 문제
+# 2차원 평면 위의 점 N개가 주어진다. 
+# 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 
+# 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+# 
+# 입력
+# 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 
+# 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. 
+# (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+# 
+# 출력
+# 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+positions = []
+
+for _ in range(int(input())):
+    positions.append(list(map(int, input().split())))
+    
+positions = sorted(
+    positions, key=lambda position: (position[0], position[1])
+)
+
+for position in positions:
+    print(position[0], position[1])
