cfbfcd90fb05c60424ae4fc838efe7eb580bc0bd
alstn2468 <alstn2468_@naver.com>
2019-06-27 13:44:47 +0900

    Solve boj 11052 buying cards

--- 11000~11099/11052.py
+++ 11000~11099/11052.py
@@ -30,3 +30,12 @@
 
 N = int(input())
 P = list(map(int, input().split()))
+dp = [0 for _ in range(N + 1)]
+
+P.insert(0, 0)
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i - j] + P[j], dp[i])
+
+print(dp[N])
