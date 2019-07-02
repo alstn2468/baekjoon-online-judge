8f71015903231fd4817d076445f9107de478085c
alstn2468 <alstn2468_@naver.com>
2019-07-02 10:15:27 +0900

    Solve 15649 boj N and M 1

--- 15600~15699/15649.py
+++ 15600~15699/15649.py
@@ -11,3 +11,25 @@
 # 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 
 # 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
 # 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+N, M = map(int, input().split())
+check = [False for _ in range(N + 1)]
+arr = [0 for _ in range(M)]
+
+def solution(idx, N, M):
+    if idx == M:
+        for i in range(M):
+            print(arr[i], end=" ")
+        print()
+        
+        return
+    
+    for i in range(1, N + 1):
+        if check[i]:
+            continue
+        
+        check[i], arr[idx] = True, i
+        solution(idx + 1, N, M)
+        check[i] = False
+        
+solution(0, N, M)
