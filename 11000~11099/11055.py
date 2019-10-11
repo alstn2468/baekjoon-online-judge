ab3715807cbe5b3fb0e9b798036adfe3a2aedc4d
김민수 <alstn2468_@naver.com>
2019-10-11 17:37:46 +0900

    Solve Boj11055 가장 큰 증가 부분 순열

--- 11000~11099/11055.py
+++ 11000~11099/11055.py
@@ -13,3 +13,16 @@
 
 N = int(input())
 arr = list(map(int, input().split()))
+dp = [arr[i] for i in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if arr[j] < arr[i] and dp[i] < dp[j] + arr[i]:
+            dp[i] = dp[j] + arr[i]
+result = 0
+
+for i in range(N):
+    if result < dp[i]:
+        result = dp[i]
+
+print(result)
