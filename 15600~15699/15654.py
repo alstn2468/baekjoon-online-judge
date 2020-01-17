c258db3986754f31ed9b1f2244fda331172bf83c
alstn2468 <alstn2468_@naver.com>
2020-01-17 11:08:01 +0900

    Solve boj15654 N과 M (5)

---
+++ 15600~15699/15654.py
@@ -0,0 +1,27 @@
+
+# 문제
+# N개의 자연수와 자연수 M이 주어졌을 때, 
+# 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+#  N개의 자연수는 모두 다른 수이다.
+# N개의 자연수 중에서 M개를 고른 수열
+#
+# 입력
+# 첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+# 둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+#
+# 출력
+# 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 
+# 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+# 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+from itertools import permutations
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+print(
+    '\n'.join([
+        ' '.join(map(str, each)) \
+            for each in sorted(list(permutations(numbers, M)))
+    ])
+)
--- README.md
+++ README.md
@@ -398,6 +398,7 @@ Department of Computer Software Engineering
 > 15650 N과 M (2)<br/>
 > 15651 N과 M (3)<br/>
 > 15652 N과 M (4)<br/>
+> 15654 N과 M (5)<br/>
 > 15740 A+B - 9<br/>
 > 15802 타노스<br/>
 > 15873 공백 없는 A+B<br>
