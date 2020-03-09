1d134f21dc84df541e8b1f70317c69b2ee45a8e1
alstn2468 <alstn2468_@naver.com>
2020-03-09 09:04:09 +0900

    Fix Boj2579 계단오르기 재채점 런타임 에러

--- 2500~2599/2579.py
+++ 2500~2599/2579.py
@@ -30,13 +30,20 @@ stair = []
 for i in range(n):
     stair.append(int(input()))
 
-sum = []
+if n < 3:
+    print(sum(stair))
 
-sum.append(stair[0])
-sum.append(stair[1] + stair[0])
-sum.append(max(stair[2] + stair[0], stair[2] + stair[1]))
+elif n == 3:
+    print(max(stair[0] + stair[2], stair[1] + stair[2]))
 
-for i in range(3, n):
-    sum.append(max(stair[i] + sum[i - 2], stair[i] + stair[i - 1] + sum[i - 3]))
+else:
+    dp = []
 
-print(sum[n - 1])
+    dp.append(stair[0])
+    dp.append(stair[1] + stair[0])
+    dp.append(max(stair[2] + stair[0], stair[2] + stair[1]))
+
+    for i in range(3, n):
+        dp.append(max(stair[i] + dp[i - 2], stair[i] + stair[i - 1] + dp[i - 3]))
+
+    print(dp[n - 1])
